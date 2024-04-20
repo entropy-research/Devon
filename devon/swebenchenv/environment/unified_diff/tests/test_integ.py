f866272953d0b303b588d85f2be4adb070627eec
killind-dev <61808204+killind-dev@users.noreply.github.com>
2024-04-20 15:38:26 -0700

    edit updates

--- devon/swebenchenv/environment/unified_diff/tests/test_integ.py
+++ devon/swebenchenv/environment/unified_diff/tests/test_integ.py
@@ -38,7 +38,7 @@ def test_diff():
 
 def test_diff_backoff_matching():
 
-    cases = ["case10", "case12", "case14"]
+    cases = ["case10", "case12", "case14", "case16"]
 
     current_file = __file__
     current_dir = os.path.dirname(current_file)
