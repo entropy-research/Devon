6d187ea328f7e671df22cf746c4dbf89524f6348
ObjectJosh <joshwong74@gmail.com>
2024-05-06 12:28:05 -0700

    chore: cleanup

--- electron/frontend/app/landing.tsx
+++ electron/frontend/app/landing.tsx
@@ -1,36 +1,21 @@
 'use client'
-import { useState, useEffect } from 'react'
-// Landing page to prompt user for project directory and API key(s)
-// import { fromEvent } from 'file-selector'
-import { useRouter, usePathname, useSearchParams } from 'next/navigation'
-// import { useRouter } from 'next/router';
-import FolderPicker from '@/components/ui/folder-picker'
-import { Button } from '@/components/ui/button'
+import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
 import { useLocalStorage } from '@/lib/hooks/chat.use-local-storage'
-import { nanoid } from '@/lib/chat.utils'
 import Home from './home'
 import OnboardingModal from '@/components/modals/onboarding-modal'
 import useStartSession from '@/lib/services/sessionService/use-start-session'
-// import useFetchSessionEvents from '@/lib/services/sessionService/use-fetch-session-events'
-import useCreateSession from '@/lib/services/sessionService/use-create-session'
-
 
 export default function Landing({ chatProps }) {
     const searchParams = useSearchParams()
-
     const chatId = searchParams.get('chat')
-    const [folderPath, setFolderPath] = useState('')
-    // const [_, setChatId] = useLocalStorage('chatId', chatId)
     const [hasAcceptedCheckbox, setHasAcceptedCheckbox] = useLocalStorage(
         'hasAcceptedCheckbox',
         false
     )
-    // const [initialized, setInitialized] = useState(true)
     const { startSession, sessionStarted, error, loading } = useStartSession()
-    const { createSession, sessionId, loading: createSessionLoading, error: createSessionError } = useCreateSession()
 
-    // const { data: events, isLoading, isError, error } = useFetchSessionEvents(sessionId);
-
+    // Basically listens for change
     useEffect(() => {
         if (loading) {
             return
