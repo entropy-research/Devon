4be185db0c4907634d15e4adbedae1dabfb78cf8
ObjectJosh <joshwong74@gmail.com>
2024-06-23 22:45:53 -0700

    feat: loads in project dir in editor & file tree

--- electron/src/backend/plugins/editor.ts
+++ electron/src/backend/plugins/editor.ts
@@ -1,14 +1,47 @@
 import { ipcMain } from 'electron'
 import * as fsPromise from 'fs/promises'
+import path from 'path'
 
 const parcelWatcher = require('@parcel/watcher')
 
 ipcMain.handle('watch-dir', async (event, dirPath) => {
     const fileContents = new Map()
-    const listeners: any = []
+
+    const readDirectory = async (dir: string): Promise<any> => {
+        const files = await fsPromise.readdir(dir, { withFileTypes: true })
+        const fileEvents = []
+
+        for (const file of files) {
+            const filePath = path.join(dir, file.name)
+            if (file.isDirectory()) {
+                fileEvents.push(...(await readDirectory(filePath)))
+            } else {
+                try {
+                    const content = await fsPromise.readFile(filePath, 'utf8')
+                    fileContents.set(filePath, content)
+                    fileEvents.push({
+                        type: 'create',
+                        path: filePath,
+                        newContent: content,
+                    })
+                } catch (err) {
+                    console.error(`Error reading file ${filePath}:`, err)
+                }
+            }
+        }
+
+        return fileEvents
+    }
 
     try {
-        console.log('got here')
+        // Initial read of the directory
+        const initialEvents = await readDirectory(dirPath)
+
+        // Give some time for event listener to establish
+        setTimeout(() => {
+            event.sender.send('file-changes', initialEvents)
+        }, 1000)
+
         const subscription = await parcelWatcher.subscribe(
             dirPath,
             async (err: any, events: any) => {
@@ -25,19 +58,17 @@ ipcMain.handle('watch-dir', async (event, dirPath) => {
                                 e.path,
                                 'utf8'
                             )
-                            console.log(newContent)
                             if (fileContents.get(e.path) !== newContent) {
                                 fileContents.set(e.path, newContent)
-                                // listeners?.forEach(l => l(e.path, newContent))
+                                updatedEvents.push({ ...e, newContent })
                             }
-                            updatedEvents.push({ ...e, newContent })
                         } catch (readErr) {
                             console.error(
                                 `Error reading file ${e.path}:`,
                                 readErr
                             )
                         }
-                    } else {
+                    } else if (e.type === 'create' || e.type === 'delete') {
                         updatedEvents.push(e)
                     }
                 }
--- electron/src/frontend/panels/editor/lib/hooks/use-file-watcher.ts
+++ electron/src/frontend/panels/editor/lib/hooks/use-file-watcher.ts
@@ -9,7 +9,7 @@ import { File } from '@/lib/types'
 type FileEvent = {
     type: 'create' | 'update' | 'delete'
     path: string
-    newContent: string
+    newContent?: string
 }
 
 const useFileWatcher = (initialFiles: File[], dirPath: string) => {
@@ -20,59 +20,46 @@ const useFileWatcher = (initialFiles: File[], dirPath: string) => {
             if (!dirPath) {
                 return
             }
+            console.log('Starting file watcher', dirPath)
             const success = await window.api.invoke('watch-dir', dirPath)
             if (!success) {
                 console.error('Failed to start watching directory')
                 return
             }
 
-            window.api.receive(
-                'file-changes',
-                (events, _events: FileEvent[]) => {
-                    // console.log('event', events)
-                    console.log('File changes:', events)
-                    const updatedFiles = [...files]
-                    events.forEach((event: FileEvent) => {
-                        console.log('new content', event.newContent)
-                        const fileIndex = updatedFiles.findIndex(
-                            file => file.path === event.path
-                        )
+            window.api.receive('file-changes', (events: FileEvent[]) => {
+                const updatedFiles = [...files]
+                events.forEach((event: FileEvent) => {
+                    const fileIndex = updatedFiles.findIndex(
+                        file => file.path === event.path
+                    )
 
-                        if (
-                            event.type === 'create' ||
-                            event.type === 'update'
-                        ) {
-                            const file = {
-                                id: event.path,
-                                name:
-                                    event.path.split('/').pop() ??
-                                    'unnamed_file',
-                                path: event.path,
-                                language: getLanguageFromFilename(
-                                    event.path.split('/').pop()
-                                ),
-                                value: { lines: event.newContent }, // You might want to read the file content here
-                                icon: getIconFromFilename(
-                                    event.path.split('/').pop()
-                                ),
-                            }
-
-                            if (fileIndex === -1) {
-                                updatedFiles.push(file)
-                            } else {
-                                updatedFiles[fileIndex] = file
-                            }
-                        } else if (
-                            event.type === 'delete' &&
-                            fileIndex !== -1
-                        ) {
-                            updatedFiles.splice(fileIndex, 1)
+                    if (event.type === 'create' || event.type === 'update') {
+                        const file = {
+                            id: event.path,
+                            name: event.path.split('/').pop() ?? 'unnamed_file',
+                            path: event.path,
+                            language: getLanguageFromFilename(
+                                event.path.split('/').pop()
+                            ),
+                            value: { lines: event.newContent || '' }, // Read new content if available
+                            icon: getIconFromFilename(
+                                event.path.split('/').pop()
+                            ),
                         }
-                    })
 
-                    setFiles(updatedFiles)
-                }
-            )
+                        if (fileIndex === -1) {
+                            updatedFiles.push(file)
+                        } else {
+                            updatedFiles[fileIndex] = file
+                        }
+                    } else if (event.type === 'delete' && fileIndex !== -1) {
+                        updatedFiles.splice(fileIndex, 1)
+                    }
+                })
+
+                setFiles(updatedFiles)
+            })
 
             return () => {
                 window.api.send('unsubscribe')
@@ -81,7 +68,7 @@ const useFileWatcher = (initialFiles: File[], dirPath: string) => {
         }
 
         startWatching()
-    }, [dirPath, files])
+    }, [dirPath])
 
     return files
 }
